8ab34d198235d2488dc738e355ab02fafc17ecdf
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-03 18:16:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,7 @@ def summon_captain_planet(planeteer_calls)
    puts "#{calls.capitalize}!"
  end
  p new array
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
+ 
 end
 
 def long_planeteer_calls(planeteer_calls)
