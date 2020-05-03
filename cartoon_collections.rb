cbaad8d4f9bf49979daaff54ee71f8dc39af99ab
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-03 18:25:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
  planeteer_calls.find_index do |calls|
    calls == valid_calls
+   p calls
  end
  
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
